62165e0829703e33cf99afd3f69053ddff64455c
Bryan Mahoney <bryan@goDynamo.com>
2013-08-01 08:47:03 -0400

    Ensures that blank line items aren't printed on the shipping label

--- lib/spree_shipworks/xml.rb
+++ lib/spree_shipworks/xml.rb
@@ -129,6 +129,7 @@ module SpreeShipworks
 
           order_context.element 'Items' do |items_context|
             self.line_items.each do |item|
+              next if item.quantity == 0
               item.extend(LineItem)
               item.to_shipworks_xml(items_context) if item.variant.present?
             end
