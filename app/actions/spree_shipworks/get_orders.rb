dab01af79a406a8538d26a1a5c9ab90594a84d31
Maksim Shilov <storm25132513@gmail.com>
2020-02-25 11:18:09 +0300

    fix double file requiring

--- app/actions/spree_shipworks/get_orders.rb
+++ app/actions/spree_shipworks/get_orders.rb
@@ -1,4 +1,4 @@
-require 'spree_shipworks/orders'
+# frozen_string_literal: true
 
 module SpreeShipworks
   class GetOrders
@@ -12,12 +12,12 @@ module SpreeShipworks
           end
         end
       end
-    rescue ArgumentError => error
-      error_response("INVALID_VARIABLE", error.to_s + "\n" + error.backtrace.join("\n"))
-    rescue => error
-      Rails.logger.error(error.to_s)
-      Rails.logger.error(error.backtrace.join("\n"))
-      error_response("INTERNAL_SERVER_ERROR", error.to_s)
+    rescue ArgumentError => e
+      error_response('INVALID_VARIABLE', e.to_s + "\n" + e.backtrace.join("\n"))
+    rescue StandardError => e
+      Rails.logger.error(e.to_s)
+      Rails.logger.error(e.backtrace.join("\n"))
+      error_response('INTERNAL_SERVER_ERROR', e.to_s)
     end
   end
 end
