c00283a9589f7b64f56cf5bd7490efb68d773494
Maksim Shilov <storm25132513@gmail.com>
2020-02-25 10:42:59 +0300

    remove unnecessary monkeypatch, replace _decorator pattern with module prepend

--- app/models/spree/number_generator_decorator.rb
+++ app/models/spree/number_generator_decorator.rb
@@ -1,29 +1,39 @@
-Spree::Core::NumberGenerator.class_eval do
-  NUMBER_LENGTH  = 9
-  NUMBER_LETTERS = false
-  NUMBER_PREFIX  = 'N'
+# frozen_string_literal: true
 
-  def generate_number(options = {})
-    options[:length]  ||= NUMBER_LENGTH
-    options[:letters] ||= NUMBER_LETTERS
-    options[:prefix]  ||= NUMBER_PREFIX
+module Spree
+  module Core
+    module NumberGeneratorDecorator
+      NUMBER_LENGTH  = 9
+      NUMBER_LETTERS = false
+      NUMBER_PREFIX  = 'N'
 
-    possible = (0..9).to_a
-    possible += ('A'..'Z').to_a if options[:letters]
+      def generate_number(options = {})
+        options[:length]  ||= NUMBER_LENGTH
+        options[:letters] ||= NUMBER_LETTERS
+        options[:prefix]  ||= NUMBER_PREFIX
 
-    self.number ||= loop do
-      # Make a random number.
-      random = "#{(0...options[:length]).map { possible.shuffle.first }.join}"
-      # make sure that first element is not zero
-      random[0] = "#{(1..9).to_a.shuffle.first}"
-      random = options[:prefix] + random
-      # Use the random  number if no other order exists with it.
-      if self.class.exists?(number: random)
-        # If over half of all possible options are taken add another digit.
-        options[:length] += 1 if self.class.count > (10 ** options[:length] / 2)
-      else
-        break random
+        possible = (0..9).to_a
+        possible += ('A'..'Z').to_a if options[:letters]
+
+        self.number ||= loop do
+          # Make a random number.
+          random = (0...options[:length]).map { possible.sample }.join.to_s
+          # make sure that first element is not zero
+          random[0] = (1..9).to_a.sample.to_s
+          random = options[:prefix] + random
+          # Use the random  number if no other order exists with it.
+          if self.class.exists?(number: random)
+            # If over half of all possible options are taken add another digit.
+            if self.class.count > (10**options[:length] / 2)
+              options[:length] += 1
+            end
+          else
+            break random
+          end
+        end
       end
     end
   end
 end
+
+Spree::Core::NumberGenerator.prepend(Spree::Core::NumberGeneratorDecorator)
--- lib/spree_shipworks/engine.rb
+++ lib/spree_shipworks/engine.rb
@@ -1,34 +1,21 @@
+# frozen_string_literal: true
+
 module SpreeShipworks
   class Engine < Rails::Engine
     require 'spree/core'
-    #isolate_namespace Spree
+    # isolate_namespace Spree
     engine_name 'spree_shipworks'
     isolate_namespace SpreeShipworks
 
-    config.autoload_paths += %W(#{config.root}/lib)
+    config.autoload_paths += %W[#{config.root}/lib]
 
     # use rspec for tests
     config.generators do |g|
       g.test_framework :rspec
     end
 
-    def self.activate
-      Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
-        Rails.configuration.cache_classes ? require(c) : load(c)
-      end
-    end
-
-    config.to_prepare &method(:activate).to_proc
-
-    initializer "spree.shipworks.environment", :before => :load_config_initializers do |app|
+    initializer 'spree.shipworks.environment', before: :load_config_initializers do |_app|
       SpreeShipworks::Config = Spree::ShipworksConfiguration.new
     end
-
-    config.to_prepare do
-      Dir.glob(Rails.root + "app/decorators/**/*_decorator*.rb").each do |c|
-        require_dependency(c)
-      end
-    end
-
   end
 end
